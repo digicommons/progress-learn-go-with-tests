825ca9328a05ee1a7af4e6b089dcbef190d8b9d0
digicommons <43313518+digicommons@users.noreply.github.com>
2023-03-25 11:30:32 +0100

    Initial commit

---
+++ hello-world/hello.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+const czech = "Czech"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const czechHelloPrefix = "Ahoj, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case czech:
+		prefix = czechHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
